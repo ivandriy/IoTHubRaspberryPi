cf9598fdf9e612aa2766262eabde82b69e76633c
Andrii Ivanskiy <andrii@ivanskiy.com>
2017-10-31 09:04:22 +0200

    Climate control app

---
+++ run_myapp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "=============================="
+echo "-----------------------------"
+date
+echo "-----------------------------"
+/home/pi/azure/iot-hub-python-raspberrypi-client-app/myapp.py
+echo "=============================="
